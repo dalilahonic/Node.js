285e8c3d0bdc4f6f95b236d2344015b3df280d75
dalilahonic <dalilahonic1@gmail.com>
2024-02-10 22:55:57 +0100

    .

--- Express.js/routes/admin.js
+++ Express.js/routes/admin.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/add-product', (req, res, next) => {
+  console.log('In the another middleware!');
+  // res.send('<h1> Add product page </h1>');
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit"> Add product </button></form>'
+  );
+});
+
+router.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 module.exports = router;
