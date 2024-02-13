f1db302aa9a443ff88fc9f20457367d97d60a6d6
dalilahonic <dalilahonic1@gmail.com>
2024-02-13 18:06:29 +0100

    .

--- working-with-dynamic-content-adding-templating-engines/routes/shop.js
+++ working-with-dynamic-content-adding-templating-engines/routes/shop.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
@@ -13,6 +12,9 @@ router.get('/', (req, res, next) => {
     prods: products,
     pageTitle: 'Shop',
     path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
