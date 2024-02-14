48eb13ec4574b2b51f9415c24628e619e8d0cd0a
dalilahonic <dalilahonic1@gmail.com>
2024-02-14 16:14:32 +0100

    .

--- Model View Controller/app.js
+++ Model View Controller/app.js
@@ -5,8 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const getErrorPage = require('./controllers/error');
 
 app.set('view engine', 'ejs');
 
@@ -15,14 +16,9 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.router);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    path: '',
-  });
-});
+app.use(getErrorPage);
 
 app.listen(3000);
