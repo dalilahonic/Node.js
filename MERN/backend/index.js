420129e3dcac4165fa15d77d432938bf47a8831a
dalilahonic <dalilahonic1@gmail.com>
2024-02-24 14:17:54 +0100

    .

--- MERN/backend/index.js
+++ MERN/backend/index.js
@@ -40,7 +40,6 @@ app.post('/books', async (req, res) => {
 });
 
 // get all books from a databse
-
 app.get('/books', async (req, res) => {
   try {
     const books = await Book.find();
@@ -50,7 +49,77 @@ app.get('/books', async (req, res) => {
       .json({ count: books.lengtg, data: books });
   } catch (err) {
     console.log(err);
-    res.status(500).send({ message: error.message });
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// get one book from database by id
+app.get('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// update a book
+app.put('/books/:id', async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message:
+          'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(
+      id,
+      req.body
+    );
+
+    if (!result) {
+      return res
+        .status(404)
+        .json({ message: 'Book not found' });
+    }
+    return res
+      .status(200)
+      .send({ message: 'Book updated successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Delete a book
+
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res
+        .status(404)
+        .json({ message: 'Book not found' });
+    }
+
+    return res
+      .status(200)
+      .send({ message: 'Book deleted successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.message });
   }
 });
 
