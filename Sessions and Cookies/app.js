d78d19f3e65e80f5813f578200c5c772d1d13fc3
dalilahonic <dalilahonic1@gmail.com>
2024-02-26 19:37:54 +0100

    .

--- Sessions and Cookies/app.js
+++ Sessions and Cookies/app.js
@@ -2,15 +2,21 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(
+  session
+);
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+require('dotenv').config();
 const mongodbUrl = process.env.URL;
 
 const app = express();
-
+const store = new MongoDBStore({
+  uri: mongodbUrl,
+  collection: 'sessions',
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -25,8 +31,21 @@ app.use(
     secret: 'my secret',
     resave: false,
     saveUninitialized: false,
+    store: store,
   })
 );
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
@@ -63,3 +82,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// "body-parser": "^1.18.3",
+// "connect-mongodb-session": "^5.0.0",
+// "dotenv": "^16.4.5",
+// "ejs": "^2.6.1",s
+// "express": "^4.16.3",
+// "express-session": "^1.18.0",
+// "mongodb": "^3.1.6",
+// "mongoose": "^5.2.17",
+// "mysql2": "^1.6.1",
+// "sequelize": "^5.0.0-beta.11"
+// "nodemon": "^1.18.3"
--- Sessions and Cookies/controllers/auth.js
+++ Sessions and Cookies/controllers/auth.js
@@ -1,18 +1,37 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie')); // loggedIn=true
-  const isLoggedIn =
-    req.get('Cookie').split('=')[1] === 'true';
-  console.log(isLoggedIn);
+  console.log(req.session);
+  // Session {
+  //   cookie: { path: '/', _expires: null, originalMaxAge: null, httpOnly: true }
+  // }
+
+  // console.log(req.session.isLoggedIn);
 
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
-exports.postLogin = (req, res) => {
-  // req.isLoggedIn = true;
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  res.redirect('/');
+module.exports.postLogin = (req, res, next) => {
+  User.findById('65db4cfbd60c28519849880d')
+    .then((user) => {
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      req.session.save((err) => {
+        if (err) console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    else console.log('Session destroyed');
+    res.redirect('/');
+  });
 };
