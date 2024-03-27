244d0e2422de81696002c0640106e2e1621bf2fc
dalilahonic <dalilahonic1@gmail.com>
2024-03-27 15:52:49 +0100

    .

--- Mongoose-Practice/index.js
+++ Mongoose-Practice/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userProfile from './schema.js';
+import { Person, UserProfile } from './schema.js';
 
 dotenv.config();
 
@@ -9,22 +9,22 @@ const URI = process.env.URI;
 
 const app = express();
 
-// const user = new userProfile();
+// const user = new UserProfile();
+// user.username = 'str'; // ValidationError: userProfile validation failed: username: Username must be longer than 5 characters
 // user.save();
-// console.log(user);
+// console.log(user); //  social: {}, when minimize set to false
+// console.log(user.$isEmpty('socials')); // true, check whether an object is empty
 
-// userProfile
-//   .create({
-//     username: 'username',
-//     social: {
-//       following: 100,
-//       followers: 100,
-//     },
-//     posts: [{ title: 'Naslov', body: 'desc' }],
-//   })
-//   .then((user) => {
-//     console.log(user);
-//   });
+// UserProfile.create({
+//   username: 'username',
+//   social: {
+//     following: 100,
+//     followers: 100,
+//   },
+//   posts: [{ title: 'Naslov', body: 'desc' }],
+// }).then((user) => {
+//   console.log(user);
+// });
 
 // user
 //   .findByUsername()
@@ -37,9 +37,90 @@ const app = express();
 
 // dalila.hello();
 
-// userProfile
+// UserProfile
 //   .findByUsername('Dalila')
 //   .then((user) => console.log(user));
+
+// UserProfile
+//   .find()
+//   .byUsername('username')
+//   .exec()
+//   .then((user) => console.log(user));
+
+// const Dalila = new Person({
+//   firstName: 'Dalila',
+//   lastName: 'Honić',
+// });
+
+// console.log(Dalila.getFullName); // Dalila Honić
+
+// const newPerson = new Person();
+// newPerson.getFullName = 'Dalila Honic';
+// console.log(newPerson);
+// {
+//   _id: new ObjectId('66033dda89f2bed59369c1c2'),
+//   firstName: 'Dalila',
+//   lastName: 'Honic'
+// }
+
+// const newPerson2 = new Person({ name: 'John' });
+// newPerson2.save();
+// console.log(newPerson2.id); // undefined, when id is set to false
+// console.log(newPerson2); // { n: 'John'}
+// console.log(newPerson2.name); // John
+// console.log(newPerson2.n); // John
+// console.log(newPerson2.toObject({ virtuals: true }));
+// {
+//   n: 'John',
+// name: 'John',
+// ....
+// }
+
+// newPerson2.name = 'New Name';
+// console.log(newPerson2); // { n: 'New Name'}
+
+// const newPerson3 = new Person({ property: 'value' }); //StrictModeError: Field `property` is not in schema and strict mode is set to throw.
+
+// deletes the first document that matches the query
+// UserProfile.deleteOne({ username: 'Dalila' }).then(
+//   (data) => {
+//     console.log(data);
+//     // { acknowledged: true, deletedCount: 1 }
+//   }
+// );
+
+// deletes all documents that match the query
+// UserProfile.deleteMany({ username: 'Dalila' }).then(
+//   (data) => {
+//     console.log(data);
+//     // { acknowledged: true, deletedCount: 4 }
+//   }
+// );
+
+// UserProfile.where('username')
+//   .equals('Dalila')
+//   .then((user) => console.log(user));
+// find a user where 'username' is equal to 'Dalila'\
+
+// const user1 = new UserProfile({
+//   age: 32,
+//   usrename: 'nekoIme',
+//   email: 'nekiemailnpr',
+// });
+
+// user1.save();
+
+UserProfile.where('age')
+  .gt('20')
+  .lte('31')
+  // .limit(2) // limit to two users
+  // .select('age username') // only get the age and username fields
+  .populate('reletedPerson') // populate reletedPerson field
+  .then((users) => {
+    console.log(users[0]);
+  });
+// finds users where age is greater than 20 and less then 31
+// you can keep chaingin these
 
 mongoose.connect(URI).then(() => {
   console.log('connected to mongodb');
--- Mongoose-Practice/schema.js
+++ Mongoose-Practice/schema.js
@@ -4,20 +4,54 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    username: { type: String, default: 'Dalila' },
-    email: String,
+    username: {
+      type: String,
+      default: 'Dalila',
+
+      // validate defines custom validation rules for the field. accepts object with 2 properties: validator, message.
+      // validation runs only when you use create or save method
+      validate: {
+        //validator recives value of a filed and returns true if value is valid
+        validator: (v) => v.length > 5,
+
+        // funtion that returns custom error message if the validation fails
+        message: (props) =>
+          `Username must be longer than 5 characters`,
+      },
+    },
+    email: {
+      type: String,
+      // required: true,
+      lowercase: true,
+      immutable: true, // the value of 'email' field cannot be changed
+      minlength: 10, // minimum allowed length of a string
+      maxlength: 100,
+    },
+    age: {
+      type: Number,
+      min: 12,
+      max: 100,
+    },
     private: Boolean,
     posts: [
       {
         //   _id: false,
         title: String,
         body: String,
-        date: { type: Date, default: Date.now },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
       },
     ],
     social: {
       following: Number,
       followers: Number,
+    },
+    reletedPerson: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Person',
+      // referencing Person model
     },
     //   _id: false, // disable id
     //   _id: Number, // overwrite Mongoose's default id
@@ -26,6 +60,7 @@ const userSchema = new Schema(
     timestamps: true, // enable automatic timestamps, track when documents are created and updated
     strict: true, // enforce strict mode, Mongoose will throw an error if you try to save a document with properties that aren't defined in the schema
     collection: 'profiles', //  specify the name of the collection in the database, by default  Mongoose pluralizes the model name to determine the collection name
+    minimize: false,
 
     // instance methods
     methods: {
@@ -43,6 +78,12 @@ const userSchema = new Schema(
       },
     },
     // static methods are the ones that are attached to the model itself (ex. userProfile). instance methods are functions that are attached to the individual document instances
+
+    query: {
+      byUsername(username) {
+        return this.where({ username });
+      },
+    },
   }
 );
 
@@ -69,7 +110,6 @@ userSchema.method('hello', function () {
   console.log('hello world!');
 });
 
-
 // another two ways to define static methods
 
 // #1
@@ -86,9 +126,64 @@ userSchema.method('hello', function () {
 
 // do not use arrow function bacause arrow functions prevent binding this
 
-const userProfile = mongoose.model(
+// query helper functions
+
+// userSchema.query.byUsername = function (username) {
+//   return this.where({ username });
+// };
+
+export const UserProfile = mongoose.model(
   'userProfile',
   userSchema
 );
 
-export default userProfile;
+//................................
+
+const personSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    n: {
+      type: String,
+      alias: 'name',
+    },
+  },
+  {
+    // virtuals in Mongoose are additional properties that you can define on your schema, but they don't actually get stored in the MongoDB database
+    virtuals: {
+      getFullName: {
+        get() {
+          return `${this.firstName} ${this.lastName}`;
+        },
+        set(fullName) {
+          this.firstName = fullName.split(' ')[0];
+          this.lastName = fullName.split(' ')[1];
+        },
+      },
+    },
+
+    // options
+    collection: 'people',
+    // autoCreate: false,
+    // caped: { size: 1024 }, // capped collection is a fixed sized collection that automaticlly overwrites the oldest documents when the collection reaches its maximum size. size specifies the maximum size of collection in bytes
+
+    capped: { size: 1024, max: 1000 }, // collection can hold maximum of 1000 documents
+    id: false,
+    minimize: false, // Mongoose by default minimizes schemas by removing empty objects. this behaviour can be overriden by setting minimize to false
+
+    strict: 'throw', // will cause error instead of dropping bad data
+  }
+);
+
+// personSchema.virtual('getFullName').get(function () {
+//   return `${this.firstName} ${this.lastName}`;
+// });
+
+// another way to set options
+// schema.set(option, value);
+// schema.set('bufferComands', false);
+
+export const Person = mongoose.model(
+  'Person',
+  personSchema
+);
