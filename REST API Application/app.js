bba6e69b7ea2a5070988f1fd77822f6a3b539e86
dalilahonic <dalilahonic1@gmail.com>
2024-04-12 14:05:20 +0100

    .

--- REST API Application/app.js
+++ REST API Application/app.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+dotenv.config();
+
+const URI = process.env.URI;
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'images/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single('image')
+);
+app.use(
+  '/images',
+  express.static(path.join(__dirname, 'images'))
+);
 
 app.use((req, res, next) => {
   res.setHeader('Access-Contorl-Allow-Origin', '*');
@@ -22,4 +62,16 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const statusCode = error.statusCode || 500;
+  const message = error.message;
+  res.status(statusCode).json({ message });
+});
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- REST API Application/controllers/feed.js
+++ REST API Application/controllers/feed.js
@@ -1,30 +1,90 @@
+const { validationResult } = require('express-validator');
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is a first post',
-        imageUrl: 'images/code.jpg',
-        creator: {
-          name: 'Dalila',
-        },
-        date: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: 'Fetched posts successfully',
+        posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
-exports.postPosts = (req, res, next) => {
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error(
+      'Validation failed, entered data is incorrect'
+    );
+    error.statusCode = 422;
+    throw error;
+
+    // return res.status(422).json({
+    //   message:
+    //     'Validation failed, entered data is incorrect',
+    //   errors: errors.array(),
+    // });
+  }
+
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace('\\', '/');
   const title = req.body.title;
   const content = req.body.content;
-  // db
-  res.status(201).json({
-    message: 'Post created successfully',
-    post: {
-      id: new Date().toISOString(),
-      title,
-      content,
-    },
+
+  const post = new Post({
+    title,
+    content,
+    imageUrl,
+    creator: { name: 'Dalila' },
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'Post created successfully',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res
+        .status(400)
+        .json({ message: 'Post fetched', post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
